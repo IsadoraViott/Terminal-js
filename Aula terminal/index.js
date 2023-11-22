d061acdacd8de840961cff9ea397c459af9892a2
IsadoraViott <133915056+IsadoraViott@users.noreply.github.com>
2023-11-22 15:19:10 -0300

    Add files via upload

---
+++ Aula terminal/index.js
@@ -0,0 +1,93 @@
+// let nomePersonagem = "Isadora"
+
+// let sobrenomePersonagem = "Viott"
+
+// let idadePersonagem = "15"
+
+// let empregoPersonagem = "Sim"
+
+// console.log("Nome é", nomePersonagem, "e meu sobrenome é", sobrenomePersonagem, "minha idade é", idadePersonagem, "e eu estou empregada:", empregoPersonagem)
+
+// console.log(`Meu nome é ${nomePersonagem} e meu sobrenome é ${sobrenomePersonagem} minha idade é ${idadePersonagem} anos. Estou empregada: ${empregoPersonagem}`)
+
+// console.log(nomePersonagem);
+// console.log(sobrenomePersonagem);
+// console.log(idadePersonagem);
+// console.log(empregoPersonagem);
+
+
+// /---------------------------------------------------------------
+
+// const nomeUsuario = prompt("Digite o seu nome")
+
+
+// const idadeUsuario = Number(prompt("Digite a sua idade"))
+// console.log(typeof idadeUsuario)
+
+// idadeUsuario.toString()
+// console.log(typeof idadeUsuario)
+
+
+// console.log(typeof nomeUsuario)
+
+// /----------------------------------------------------
+
+let nome 
+
+let cidade 
+
+console.log(typeof nome)
+
+console.log(typeof cidade)
+
+nome = String(prompt("Digite seu nome"))
+
+console.log(typeof nome)
+
+cidade = String(prompt("Digite a cidade onde mora"))
+
+console.log(`Olá, meu nome é ${nome} e eu moro em ${cidade}`)
+
+// // /---------------------------------------------------------
+
+let pergunta1
+
+let pergunta2
+
+let pergunta3
+
+pergunta1 = String(prompt("Você gosta de programar?(sim)(não)"))
+
+pergunta2 = String(prompt("Você já viajou para fora do país?(sim)(não)"))
+
+pergunta3 = String(prompt("Você tem animais de estimação?(sim)(não)"))
+
+console.log(`Você gosta de programar? ${pergunta1}! Você já viajou para fora do país? ${pergunta2}! Você tem animais de estimação? ${pergunta3}!`)
+
+// // /----------------------------------------------------------
+
+let x = 10
+let y = 20
+
+let novo1 = y
+
+let novo2 = x
+
+console.log(`x = ${novo1} y = ${novo2}`)
+
+// /---------------------------------------------------------------
+
+let numero1 
+
+let numero2
+
+numero1 = Number(prompt("Escolha um número"))
+
+numero2 = Number(prompt("Escolha um número"))
+
+x = numero1 + numero2
+
+y = numero1 * numero2
+
+console.log(`A soma dos números é: ${x}
+A multiplicação dos números é: ${y}`)
